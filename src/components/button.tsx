c4709b44ef79961a325ae19a90ef565038ddbac7
Manraj <manraj2712@gmail.com>
2023-10-07 02:47:44 +0530

    button bg-color param added

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -24,17 +24,21 @@ export default function Button({
 }: Props) {
   return (
     <button
-      className="flexCenter gap-3 px-4 py-3 rounded-lg bg-primary-purple text-white"
       type={type || "button"}
-      disabled={isSubmitting}
+      disabled={isSubmitting || false}
+      className={`flexCenter gap-3 px-4 py-3 
+        ${textColor ? textColor : "text-white"} 
+        ${
+          isSubmitting ? "bg-black/50" : bgColor ? bgColor : "bg-primary-purple"
+        } rounded-xl text-sm font-medium max-md:w-full`}
       onClick={handleClick}
     >
       {leftIcon && (
-        <Image src={leftIcon} alt="leftIcon" width={14} height={14} />
+        <Image src={leftIcon} width={14} height={14} alt="left icon" />
       )}
       {title}
       {rightIcon && (
-        <Image src={rightIcon} alt="rightIcon" width={14} height={14} />
+        <Image src={rightIcon} width={14} height={14} alt="right icon" />
       )}
     </button>
   );
